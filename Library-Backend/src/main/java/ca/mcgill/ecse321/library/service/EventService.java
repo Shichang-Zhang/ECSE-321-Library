05d62f1f2014e3d2fc329a879f0154b0f8036360
Shichang-Zhang <77728761+Shichang-Zhang@users.noreply.github.com>
2021-12-28 17:22:09 -0500

    init problem

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/EventService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/EventService.java
@@ -30,9 +30,9 @@ public class EventService {
      */
     @Transactional
     public void initEvent() {
-        this.createEvent("event1", Date.valueOf("2021-12-20"), Date.valueOf("2021-12-21"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
-        this.createEvent("event2", Date.valueOf("2021-12-22"), Date.valueOf("2021-12-23"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
-        this.createEvent("event3", Date.valueOf("2021-12-24"), Date.valueOf("2021-12-25"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
+        this.createEvent("event1", Date.valueOf("2022-12-20"), Date.valueOf("2022-12-21"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
+        this.createEvent("event2", Date.valueOf("2022-12-22"), Date.valueOf("2022-12-23"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
+        this.createEvent("event3", Date.valueOf("2022-12-24"), Date.valueOf("2022-12-25"), Time.valueOf("10:00:00"), Time.valueOf("12:00:00"));
     }
 
     /**
